8e016ed9385cb65712f433d6255728c6ef3ebcd7
Vladimir Trofimenko <psdmaster.ru@gmail.com>
2024-01-04 23:18:00 +0300

    Added annotations for index and products function

--- store/products/views.py
+++ store/products/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render
+from django.http import HttpRequest, HttpResponse
 
 
-def index(request):
+def index(request: HttpRequest) -> HttpResponse:
     return render(request, 'products/index.html')
 
 
-def products(request):
+def products(request: HttpRequest) -> HttpResponse:
     return render(request, 'products/products.html')
