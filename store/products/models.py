1ce25493452c6c283f386cad85849dc6dc2486b5
Vladimir Trofimenko <psdmaster.ru@gmail.com>
2024-01-07 01:23:32 +0300

    Add models for categories and products

--- store/products/models.py
+++ store/products/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+
+class ProductCategory(models.Model):
+    name = models.CharField(max_length=128, unique=True, verbose_name='Название категории')
+    description = models.TextField(null=True, blank=True, verbose_name='Описание категории')
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=256, verbose_name='Название товара')
+    description = models.TextField(verbose_name='Описание товара')
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    quantity = models.PositiveIntegerField(default=0, verbose_name='Количество товара на складе')
+    image = models.ImageField(upload_to='products_images')
+    category = models.ForeignKey(
+        ProductCategory,
+        on_delete=models.PROTECT,
+        related_name='category',
+    )
