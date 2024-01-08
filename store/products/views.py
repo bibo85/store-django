0b08287e4f479866753ca49f7d57906980878a14
Vladimir Trofimenko <psdmaster.ru@gmail.com>
2024-01-09 00:20:54 +0300

    Displaying categories and products in the template

--- store/products/views.py
+++ store/products/views.py
@@ -1,10 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpRequest, HttpResponse
 
+from products.models import Product, ProductCategory
+
 
 def index(request: HttpRequest) -> HttpResponse:
     return render(request, 'products/index.html')
 
 
 def products(request: HttpRequest) -> HttpResponse:
-    return render(request, 'products/products.html')
+    context = {
+        'title': 'Store - Каталог',
+        'products': Product.objects.all(),
+        'categories': ProductCategory.objects.all(),
+    }
+    return render(request, 'products/products.html', context=context)
