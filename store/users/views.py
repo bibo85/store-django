3074fd0557992f04bcd260de8e7fc81a7491f1ac
Vladimir Trofimenko <psdmaster.ru@gmail.com>
2024-01-11 23:59:17 +0300

    Add user authorization

--- store/users/views.py
+++ store/users/views.py
@@ -1,12 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponseRedirect
+from django.contrib import auth
+from django.urls import reverse
 
-from .models import User
 from .forms import UserLoginForm
 
 
 def login(request):
+    if request.method == 'POST':
+        form = UserLoginForm(data=request.POST)
+        if form.is_valid():
+            username = request.POST['username']
+            password = request.POST['password']
+            user = auth.authenticate(username=username, password=password)
+            if user:
+                auth.login(request, user)
+                return HttpResponseRedirect(reverse('products:products-list'))
+    else:
+        form = UserLoginForm()
     context = {
-        'form': UserLoginForm(),
+        'form': form,
     }
     return render(request, 'users/login.html', context=context)
 
