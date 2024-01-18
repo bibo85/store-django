15adb0c1e77cd9bfcb07283bd258aedfed7fdc46
Vladimir Trofimenko <psdmaster.ru@gmail.com>
2024-01-18 20:10:45 +0300

    Add verbose_name for price & category fields in Product model

--- store/products/models.py
+++ store/products/models.py
@@ -14,13 +14,14 @@ class ProductCategory(models.Model):
 class Product(models.Model):
     name = models.CharField(max_length=256, verbose_name='Название товара')
     description = models.TextField(verbose_name='Описание товара')
-    price = models.DecimalField(max_digits=8, decimal_places=2)
+    price = models.DecimalField(max_digits=8, decimal_places=2, verbose_name='Цена')
     quantity = models.PositiveIntegerField(default=0, verbose_name='Количество товара на складе')
     image = models.ImageField(upload_to='products_images')
     category = models.ForeignKey(
         ProductCategory,
         on_delete=models.PROTECT,
         related_name='category',
+        verbose_name='Категория'
     )
 
     def __str__(self):
