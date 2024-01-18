ad9d7cf8b4a6b83051a197319c1bb4555582d682
Vladimir Trofimenko <psdmaster.ru@gmail.com>
2024-01-18 21:01:10 +0300

    Add Product model in admin panel

--- store/products/admin.py
+++ store/products/admin.py
@@ -1,5 +1,12 @@
 from django.contrib import admin
 from .models import Product, ProductCategory
 
-admin.site.register(Product)
 admin.site.register(ProductCategory)
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('name', 'price', 'quantity', 'category')
+    fields = ('name', 'description', 'image', 'price', 'quantity', 'category')
+    search_fields = ('name', 'description')
+    ordering = ('-id',)
