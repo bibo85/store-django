0f7067d2524458757f8d9f5e56f340c39520c6d4
Vladimir Trofimenko <psdmaster.ru@gmail.com>
2024-02-03 23:46:21 +0300

    Refactoring func login. Add cls UserLoginView

--- store/users/urls.py
+++ store/users/urls.py
@@ -1,12 +1,12 @@
 from django.urls import path
 from django.contrib.auth.decorators import login_required
 
-from .views import login, UserRegistrationView, UserProfileView, logout
+from .views import UserLoginView, UserRegistrationView, UserProfileView, logout
 
 app_name = 'users'
 
 urlpatterns = [
-    path('login/', login, name='login'),
+    path('login/', UserLoginView.as_view(), name='login'),
     path('registration/', UserRegistrationView.as_view(), name='registration'),
     path('profile/<int:pk>', login_required(UserProfileView.as_view()), name='profile'),
     path('logout/', logout, name='logout'),
--- store/users/views.py
+++ store/users/views.py
@@ -2,28 +2,16 @@ from django.shortcuts import render, HttpResponseRedirect
 from django.contrib import auth, messages
 from django.urls import reverse, reverse_lazy
 from django.views.generic.edit import CreateView, UpdateView
+from django.contrib.auth.views import LoginView
 
 from .models import User
 from .forms import UserLoginForm, UserRegistrationForm, UserProfileForm
 from products.models import Basket
 
 
-def login(request):
-    if request.method == 'POST':
-        form = UserLoginForm(data=request.POST)
-        if form.is_valid():
-            username = request.POST['username']
-            password = request.POST['password']
-            user = auth.authenticate(username=username, password=password)
-            if user:
-                auth.login(request, user)
-                return HttpResponseRedirect(reverse('products:products-list'))
-    else:
-        form = UserLoginForm()
-    context = {
-        'form': form,
-    }
-    return render(request, 'users/login.html', context=context)
+class UserLoginView(LoginView):
+    template_name = 'users/login.html'
+    form_class = UserLoginForm
 
 
 class UserRegistrationView(CreateView):
