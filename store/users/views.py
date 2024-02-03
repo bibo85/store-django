3956973d1e5108f0cb8707ad120747e3fe7eea3d
Vladimir Trofimenko <psdmaster.ru@gmail.com>
2024-02-04 00:31:40 +0300

    Add custom mixin for the title

--- store/users/views.py
+++ store/users/views.py
@@ -8,37 +8,35 @@ from django.contrib.messages.views import SuccessMessageMixin
 from .models import User
 from .forms import UserLoginForm, UserRegistrationForm, UserProfileForm
 from products.models import Basket
+from common.views import TitleMixin
 
 
-class UserLoginView(LoginView):
+class UserLoginView(TitleMixin, LoginView):
     template_name = 'users/login.html'
     form_class = UserLoginForm
+    title = 'Store - Авторизация'
 
 
-class UserRegistrationView(SuccessMessageMixin, CreateView):
+class UserRegistrationView(SuccessMessageMixin, TitleMixin, CreateView):
     model = User
     template_name = 'users/registration.html'
     form_class = UserRegistrationForm
     success_url = reverse_lazy('users:login')
     success_message = 'Вы успешно зарегистрированы'
-
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data()
-        context['title'] = 'Store - Регистрация пользователя'
-        return context
+    title = 'Store - Регистрация пользователя'
 
 
-class UserProfileView(SuccessMessageMixin, UpdateView):
+class UserProfileView(SuccessMessageMixin, TitleMixin, UpdateView):
     model = User
     template_name = 'users/profile.html'
     form_class = UserProfileForm
     success_message = 'Данные профиля успешно обновлены'
+    title = 'Store - Профиль пользователя'
 
     def get_success_url(self):
         return reverse_lazy('users:profile', args=(self.object.id,))
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data()
-        context['title'] = 'Store - Профиль пользователя'
         context['baskets'] = Basket.objects.filter(user=self.object)
         return context
