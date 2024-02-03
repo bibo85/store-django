4b400ff4fbf4dcd4a9074a542aaf0cd225a09f86
Vladimir Trofimenko <psdmaster.ru@gmail.com>
2024-02-03 23:59:33 +0300

    Add messages about success profile create and update

--- store/users/views.py
+++ store/users/views.py
@@ -3,6 +3,7 @@ from django.contrib import auth, messages
 from django.urls import reverse, reverse_lazy
 from django.views.generic.edit import CreateView, UpdateView
 from django.contrib.auth.views import LoginView
+from django.contrib.messages.views import SuccessMessageMixin
 
 from .models import User
 from .forms import UserLoginForm, UserRegistrationForm, UserProfileForm
@@ -14,11 +15,12 @@ class UserLoginView(LoginView):
     form_class = UserLoginForm
 
 
-class UserRegistrationView(CreateView):
+class UserRegistrationView(SuccessMessageMixin, CreateView):
     model = User
     template_name = 'users/registration.html'
     form_class = UserRegistrationForm
     success_url = reverse_lazy('users:login')
+    success_message = 'Вы успешно зарегистрированы'
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data()
@@ -26,10 +28,11 @@ class UserRegistrationView(CreateView):
         return context
 
 
-class UserProfileView(UpdateView):
+class UserProfileView(SuccessMessageMixin, UpdateView):
     model = User
     template_name = 'users/profile.html'
     form_class = UserProfileForm
+    success_message = 'Данные профиля успешно обновлены'
 
     def get_success_url(self):
         return reverse_lazy('users:profile', args=(self.object.id,))
