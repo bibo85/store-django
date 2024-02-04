5170393aee026e593d6096fe239e7dd781c429e7
Vladimir Trofimenko <psdmaster.ru@gmail.com>
2024-02-04 23:38:16 +0300

    Add model EmailVerification

--- store/users/models.py
+++ store/users/models.py
@@ -4,4 +4,14 @@ from django.contrib.auth.models import AbstractUser
 
 class User(AbstractUser):
     image = models.ImageField(upload_to='users_images', null=True, blank=True, verbose_name='Аватар пользователя')
+    is_verified_email = models.BooleanField(default=False, verbose_name='Статус подтверждения email')
 
+
+class EmailVerification(models.Model):
+    code = models.UUIDField(unique=True, verbose_name='Код для подтверждения email')
+    user = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name='Пользователь')
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='Дата создания')
+    expiration = models.DateTimeField(verbose_name='Дата окончания')
+
+    def __str__(self):
+        return f'EmailVerification object for {self.user.email}'
