53ce37886f2d9fd068d6fff9e0cfea339b538c3e
Vladimir Trofimenko <psdmaster.ru@gmail.com>
2024-02-19 23:00:20 +0300

    Add tests for UserLoginView

--- store/users/tests.py
+++ store/users/tests.py
@@ -5,7 +5,7 @@ from django.test import TestCase
 from django.urls import reverse
 from django.utils.timezone import now
 
-from users.forms import UserRegistrationForm
+from users.forms import UserRegistrationForm, UserLoginForm
 from users.models import EmailVerification, User
 
 
@@ -56,3 +56,42 @@ class UserRegistrationViewTestCase(TestCase):
         response = self.client.post(self.path, self.form_data)
         self.assertEqual(response.status_code, HTTPStatus.OK)
         self.assertContains(response, 'Пользователь с таким именем уже существует.', html=True)
+
+
+class UserLoginViewTestCase(TestCase):
+    fixtures = ['socialapp.json']
+
+    def setUp(self):
+        self.user_data = {
+            'username': 'TestUser',
+            'email': 'test@test.ru',
+            'password': 'testpassword',
+        }
+        self.login_url = reverse('users:login')
+        self.user = User.objects.create_user(**self.user_data)
+
+    def test_login_page_loads_successfully(self):
+        response = self.client.get(self.login_url)
+
+        self.assertEqual(response.status_code, HTTPStatus.OK)
+        self.assertTemplateUsed(response, 'users/login.html')
+        self.assertEqual(response.context_data['title'], 'Store - Авторизация')
+        self.assertIsInstance(response.context_data['form'], UserLoginForm)
+
+    def test_login_successful(self):
+        response = self.client.post(self.login_url, self.user_data)
+        self.assertRedirects(response, reverse('index'))
+        self.assertTrue(self.client.session['_auth_user_id'])
+
+    def test_login_failure(self):
+        data = {
+            'username': self.user_data['username'],
+            'password': 'wrongpassword',
+        }
+        response = self.client.post(self.login_url, data)
+        self.assertEqual(response.status_code, HTTPStatus.OK)
+        self.assertTemplateUsed(response, 'users/login.html')
+        self.assertTrue(response.context_data['form'].errors)
+        self.assertIsInstance(response.context_data['form'], UserLoginForm)
+        self.assertContains(response, 'Пожалуйста, введите правильные имя пользователя и пароль. '
+                                      'Оба поля могут быть чувствительны к регистру.', html=True)
