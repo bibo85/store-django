a676c987bb8d1428e0b3a2b08b6a99c201f53e3d
Vladimir Trofimenko <psdmaster.ru@gmail.com>
2024-02-21 17:33:09 +0300

    Add caching for catalog page

--- store/products/urls.py
+++ store/products/urls.py
@@ -6,7 +6,7 @@ from .views import ProductsListView, basket_add, basket_remove
 app_name = 'products'
 
 urlpatterns = [
-    path('', ProductsListView.as_view(), name='products-list'),
+    path('', cache_page(60 * 30)(ProductsListView.as_view()), name='products-list'),
     path('category/<int:category_id>', cache_page(60 * 30)(ProductsListView.as_view()), name='category-products'),
     path('baskets/add/<int:product_id>/', basket_add, name='basket_add'),
     path('baskets/remove/<int:basket_id>/', basket_remove, name='basket_remove'),
