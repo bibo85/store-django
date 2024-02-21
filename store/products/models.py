3c0eb118d5ee8edf08b82991c8025cc2526c7a5b
Vladimir Trofimenko <psdmaster.ru@gmail.com>
2024-02-21 17:32:30 +0300

    Add ordering by id for Product in product.models

--- store/products/models.py
+++ store/products/models.py
@@ -31,6 +31,7 @@ class Product(models.Model):
     class Meta:
         verbose_name = 'Товар'
         verbose_name_plural = 'Товары'
+        ordering = ('id',)
 
     def __str__(self):
         return self.name
