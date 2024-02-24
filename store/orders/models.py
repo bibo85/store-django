5cd6d3bf769862b83df49d061665d235bfd9dacc
Vladimir Trofimenko <psdmaster.ru@gmail.com>
2024-02-24 16:14:29 +0300

    Create model Order

--- store/orders/models.py
+++ store/orders/models.py
@@ -1,3 +1,31 @@
 from django.db import models
 
-# Create your models here.
+from users.models import User
+
+
+class Order(models.Model):
+    CREATED = 0
+    PAID = 1
+    ON_WAY = 2
+    DELIVERED = 3
+    STATUSES = (
+        (CREATED, 'Создан'),
+        (PAID, 'Оплачен'),
+        (ON_WAY, 'В пути'),
+        (DELIVERED, 'Доставлен'),
+    )
+
+    first_name = models.CharField(max_length=64, verbose_name='Имя')
+    last_name = models.CharField(max_length=64, verbose_name='Фамилия')
+    email = models.EmailField(max_length=256, verbose_name='Электронная почта')
+    address = models.CharField(max_length=256, verbose_name='Адрес доставки')
+    basket_history = models.JSONField(default=dict)
+    created = models.DateTimeField(auto_now_add=True, verbose_name='Дата создания')
+    status = models.SmallIntegerField(default=CREATED, choices=STATUSES)
+    initiator = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+    )
+
+    def __str__(self):
+        return f'Order #{self.id}. {self.first_name} {self.last_name}'
