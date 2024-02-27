f151deefddf7a84a2aab6e1567b877ab67483fc5
Vladimir Trofimenko <psdmaster.ru@gmail.com>
2024-02-27 23:19:12 +0300

    Connecting the stripe webhook

--- store/store/urls.py
+++ store/store/urls.py
@@ -20,6 +20,7 @@ from django.contrib import admin
 from django.urls import include, path
 
 from products.views import IndexView
+from orders.views import stripe_webhook_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -28,6 +29,7 @@ urlpatterns = [
     path('users/', include('users.urls')),
     path('accounts/', include('allauth.urls')),
     path('orders/', include('orders.urls')),
+    path('webhook/stripe/', stripe_webhook_view, name='stripe_webhook'),
 ]
 
 if settings.DEBUG:
