4795f1773e3c3ffdf961bb6fbdd8c74b22035654
Vladimir Trofimenko <psdmaster.ru@gmail.com>
2024-02-28 23:45:57 +0300

    Add creation list of products for payment

--- store/orders/views.py
+++ store/orders/views.py
@@ -10,6 +10,7 @@ from django.urls import reverse_lazy, reverse
 
 from common.views import TitleMixin
 from orders.forms import OrderForm
+from products.models import Basket
 
 stripe.api_key = settings.STRIPE_SECRET_KEY
 
@@ -31,14 +32,10 @@ class OrderCreateView(TitleMixin, CreateView):
 
     def post(self, request, *args, **kwargs):
         super(OrderCreateView, self).post(request, *args, **kwargs)
+        baskets = Basket.objects.filter(user=self.request.user)
+
         checkout_session = stripe.checkout.Session.create(
-            line_items=[
-                {
-                    # Provide the exact Price ID (for example, pr_1234) of the product you want to sell
-                    'price': 'price_1Oo6LsGhMzhlobJN9ZlxupUF',
-                    'quantity': 1,
-                },
-            ],
+            line_items=baskets.stripe_products(),
             metadata={'order_id': self.object.id},
             mode='payment',
             success_url='{}{}'.format(settings.DOMAIN_NAME, reverse('orders:order_success')),
