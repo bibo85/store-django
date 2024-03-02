ddd18bd6203d31425b4c11d5a64c83e42cf0cb05
Vladimir Trofimenko <psdmaster.ru@gmail.com>
2024-03-03 02:21:46 +0300

    Add login_required to the orders app routes

--- store/orders/urls.py
+++ store/orders/urls.py
@@ -1,12 +1,13 @@
 from django.urls import path
+from django.contrib.auth.decorators import login_required
 
 from orders.views import OrderCreateView, SuccessTemplateView, CancelTemplateView, OrderListView
 
 app_name = 'orders'
 
 urlpatterns = [
-    path('order-create/', OrderCreateView.as_view(), name='order_create'),
-    path('', OrderListView.as_view(), name='orders_list'),
-    path('order-success/', SuccessTemplateView.as_view(), name='order_success'),
-    path('order-canceled/', CancelTemplateView.as_view(), name='order_canceled'),
+    path('order-create/', login_required(OrderCreateView.as_view()), name='order_create'),
+    path('', login_required(OrderListView.as_view()), name='orders_list'),
+    path('order-success/', login_required(SuccessTemplateView.as_view()), name='order_success'),
+    path('order-canceled/', login_required(CancelTemplateView.as_view()), name='order_canceled'),
 ]
