f98927766261e81dbcd58e47f69ef5750331485a
Vladimir Trofimenko <psdmaster.ru@gmail.com>
2024-03-03 19:21:50 +0300

    Output of detailed information about the order

--- store/orders/urls.py
+++ store/orders/urls.py
@@ -1,13 +1,14 @@
 from django.urls import path
 from django.contrib.auth.decorators import login_required
 
-from orders.views import OrderCreateView, SuccessTemplateView, CancelTemplateView, OrderListView
+from orders.views import OrderCreateView, SuccessTemplateView, CancelTemplateView, OrderListView, OrderDetailView
 
 app_name = 'orders'
 
 urlpatterns = [
     path('order-create/', login_required(OrderCreateView.as_view()), name='order_create'),
     path('', login_required(OrderListView.as_view()), name='orders_list'),
+    path('order/<int:pk>', login_required(OrderDetailView.as_view()), name='order_detail'),
     path('order-success/', login_required(SuccessTemplateView.as_view()), name='order_success'),
     path('order-canceled/', login_required(CancelTemplateView.as_view()), name='order_canceled'),
 ]
