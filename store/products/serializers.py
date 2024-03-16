ca92b09a0069ab4d59bdce042db112bd55e5500e
Vladimir Trofimenko <psdmaster.ru@gmail.com>
2024-03-16 17:20:10 +0300

    Added basket api for user

--- store/products/serializers.py
+++ store/products/serializers.py
@@ -1,6 +1,7 @@
 from rest_framework import serializers
+from rest_framework import fields
 
-from products.models import Product, ProductCategory
+from products.models import Product, ProductCategory, Basket
 
 
 class ProductSerializer(serializers.ModelSerializer):
@@ -9,3 +10,23 @@ class ProductSerializer(serializers.ModelSerializer):
     class Meta:
         model = Product
         fields = ('id', 'name', 'description', 'price', 'quantity', 'image', 'category')
+
+
+class BasketSerializer(serializers.ModelSerializer):
+    product = ProductSerializer()
+    sum = fields.FloatField()
+    total_sum = fields.SerializerMethodField()
+    total_quantity = fields.SerializerMethodField()
+
+    class Meta:
+        model = Basket
+        fields = ("id", "product", "quantity", "sum", "total_sum", "total_quantity", "create_timestamp")
+        read_only_fields = ("create_timestamp", )
+
+    @staticmethod
+    def get_total_sum(obj):
+        return Basket.objects.filter(user_id=obj.user).total_sum()
+
+    @staticmethod
+    def get_total_quantity(obj):
+        return Basket.objects.filter(user_id=obj.user).total_quantity()
